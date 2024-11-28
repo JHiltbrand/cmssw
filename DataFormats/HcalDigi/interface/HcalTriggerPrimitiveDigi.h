21fadc6dd45298afa8946b48d4a51eea08e81597
Joshua Hiltbrand <joshua.hiltbrand@gmail.com>
2024-11-28 15:56:24 +0100

    custom tp stuff for debug

--- DataFormats/HcalDigi/interface/HcalTriggerPrimitiveDigi.h
+++ DataFormats/HcalDigi/interface/HcalTriggerPrimitiveDigi.h
@@ -3,6 +3,8 @@
 
 #include <ostream>
 #include <vector>
+#include <array>
+#include "CalibFormats/CaloObjects/interface/IntegerCaloSamples.h"
 #include "DataFormats/HcalDetId/interface/HcalTrigTowerDetId.h"
 #include "DataFormats/HcalDigi/interface/HcalTriggerPrimitiveSample.h"
 
@@ -16,6 +18,9 @@ public:
 
   HcalTriggerPrimitiveDigi();  // for persistence
   explicit HcalTriggerPrimitiveDigi(const HcalTrigTowerDetId& id);
+
+  static const int MAXSAMPLES = 10;
+  static const int TPSAMPLES = 4;
 
   const HcalTrigTowerDetId& id() const { return id_; }
   int size() const { return (size_ & 0xF); }
@@ -41,14 +46,23 @@ public:
   void setSize(int size);
   void setPresamples(int ps);
   void setSample(int i, const HcalTriggerPrimitiveSample& sam) { data_[i] = sam; }
+  void setInputLinearFrame(const IntegerCaloSamples& inputLinearFrame);
+  void setOutputLinearFrame(const IntegerCaloSamples& outputLinearFrame);
+  void setVetoedTPs(const std::vector<uint8_t>& vetoedTPs);
 
-  static const int MAXSAMPLES = 10;
+  const std::array<uint16_t, MAXSAMPLES>& getInputLinearFrame() const { return inputLinearFrame_; }
+  const std::array<uint16_t, TPSAMPLES>& getOutputLinearFrame() const { return outputLinearFrame_; }
+  const std::array<uint8_t, TPSAMPLES>& getVetoedTPs() const { return vetoedTPs_; }
 
 private:
   HcalTrigTowerDetId id_;
   int size_;
   int hcalPresamples_;
   HcalTriggerPrimitiveSample data_[MAXSAMPLES];
+
+  std::array<uint16_t, MAXSAMPLES> inputLinearFrame_{};
+  std::array<uint16_t, TPSAMPLES> outputLinearFrame_{};
+  std::array<uint8_t, TPSAMPLES> vetoedTPs_{};
 };
 
 std::ostream& operator<<(std::ostream& s, const HcalTriggerPrimitiveDigi& digi);
